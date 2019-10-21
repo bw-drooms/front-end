26a0f18a44715dbf6d9f8556cd2e69a072ec6e6b
CScori <amilanotte1366@gmail.com>
2019-10-21 13:17:34 -0500

    dummy holders for c-land

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CmpLanding from './components/company/CmpLanding';
 
 function App() {
   return (
     <div className="App">
       <h1>Droom</h1>
+      <CmpLanding />
     </div>
   );
 }
---
+++ droom/src/components/company/CmpLanding.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CmpLanding = () => {
+    return (
+        <div className='landing-container'>
+            <nav>Home \t Post Job \t Matches</nav>
+            <div className='company-display'>
+                <div className='company-info'>
+
+                </div>
+                <div className='company-jobs'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CmpLanding
