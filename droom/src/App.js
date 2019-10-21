4023c8ec75998dd53af44c517db9fa0425d72698
CScori <amilanotte1366@gmail.com>
2019-10-21 14:55:34 -0500

    CREATING C Job Form

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import C_Landing from './components/company/C_Landing';
+import C_JobForm from './components/company/C_JobForm'
 
 function App() {
   return (
     <div className="App">
       <h1>Droom</h1>
-      <C_Landing />
+      {/* <C_Landing /> */}
+      <C_JobForm />
     </div>
   );
 }
---
+++ droom/src/components/company/C_JobForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+const C_JobForm = () => {
+    const [companyJobs, setCompanyJobs] = useState([
+        {
+            "id": 2,
+            "created_at": null,
+            "location": "Austin",
+            "position": "Software Engineer",
+            "description": "Allogy is an employee-owned SaaS company with growing presence in healthcare and government sectors. As a part of the Allogy core team you will be developing enterprise software products in collaboration with innovation teams at some of the largest hospitals and federal agencies in the country. Your job will include thinking critically and making architectural decisions focusing on longevity and maintainability. As the team grows you will have a continued say in product development decisions and the architecture roadmap for our products.",
+            "pay_range": 110000,
+            "selected": 0,
+            "company_id": 2,
+            "company_name": "Allogy",
+            "company_industry": "information technology",
+            "number_of_employees": 500
+        }
+    ])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default C_JobForm
