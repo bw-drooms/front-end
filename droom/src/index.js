629600694290fe03e374dc5604f3c99f4f442f05
CScori <amilanotte1366@gmail.com>
2019-10-21 16:36:16 -0500

    adds logger and starts store set up

--- droom/src/index.js
+++ droom/src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import { logger } from 'redux-logger'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 
 
