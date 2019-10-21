3f6368ebe1489470f44e3a807d524515e1884b87
CScori <amilanotte1366@gmail.com>
2019-10-21 15:33:02 -0500

    adds inputs for forms on CJF

--- droom/src/components/company/CJobForm.js
+++ droom/src/components/company/CJobForm.js
@@ -3,17 +3,12 @@ import React, {useState} from 'react'
 const C_JobForm = () => {
     const [companyJobs, setCompanyJobs] = useState([
         {
-            "id": 2,
-            "created_at": null,
-            "location": "Austin",
-            "position": "Software Engineer",
-            "description": "Allogy is an employee-owned SaaS company with growing presence in healthcare and government sectors. As a part of the Allogy core team you will be developing enterprise software products in collaboration with innovation teams at some of the largest hospitals and federal agencies in the country. Your job will include thinking critically and making architectural decisions focusing on longevity and maintainability. As the team grows you will have a continued say in product development decisions and the architecture roadmap for our products.",
-            "pay_range": 110000,
+            "location": "",
+            "position": "",
+            "description": "",
+            "pay_range": "",
             "selected": 0,
             "company_id": 2,
-            "company_name": "Allogy",
-            "company_industry": "information technology",
-            "number_of_employees": 500
         }
     ])
 
@@ -29,6 +24,43 @@ const C_JobForm = () => {
     return (
         <div>
             <form onSubmit={cJForm}>
+            <legend>Fill a Position</legend>
+          <label>
+            Location:
+            <input
+              type='text'
+              name='location'
+              value='handle'
+              onChange
+              />
+              </label>
+              <label>
+            Position:
+            <input
+              type='text'
+              name='position'
+              value='handle'
+              onChange
+              />
+              </label>
+              <label>
+            Salary Offer:
+            <input
+              type='text'
+              name='pay_range'
+              value='handle'
+              onChange
+              />
+              </label>
+              <label>
+            Description:
+            <input
+              type='text'
+              name='description'
+              value='handle'
+              onChange
+              />
+              </label>
                 <button type='submit'>Submit</button>
             </form>
             
