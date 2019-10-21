e0e8dd28b8d2ba16976d69d241fe451b083bc1db
CScori <amilanotte1366@gmail.com>
2019-10-21 16:45:47 -0500

    adds main reducer state and separate reducer files to break components

---
+++ droom/src/utils/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+
+const initialState = {
+    companyJob: [],
+    company: [],
+    isFetching: false,
+    isUpdating: false,
+    error: 'Error Response'
+
+}
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {           
+        default:
+            return state;
+    }
+}
