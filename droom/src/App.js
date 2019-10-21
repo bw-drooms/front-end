aa78b41c7095e0606ad30ddc6431331975db1023
CScori <amilanotte1366@gmail.com>
2019-10-21 16:01:41 -0500

    removes app state call

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-// import CLanding from './components/company/CLanding';
-import CJobForm from './components/company/CJobForm'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Droom</h1>
-      {/* <CLanding /> */}
-      <CJobForm />
+     
     </div>
   );
 }
