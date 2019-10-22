fddd1bc74e27082e4f50e9e2ec344ed5e681a28a
CScori <amilanotte1366@gmail.com>
2019-10-21 19:44:30 -0500

    request ready to be made need endpoint

--- droom/src/utils/actions/index.js
+++ droom/src/utils/actions/index.js
@@ -1,13 +1,17 @@
+import axios from 'axios'
+
 //<------------------COMPANY JOBS FETCH ---------->
 export const C_JOB_DATA = 'C_JOB_DATA'
 export const C_JOB_FETCH_SUCCESS = 'C_JOB_FETCH_SUCCESS'
 export const C_JOB_FETCH_FAILED = 'C_JOB_FETCH_FAILED '
 
-export const getJobData = () => {
-    // .get('')
-    // .then(res => {
-    //     console.log('Initial Call for jobs', res.data)
-    //     newState(res.data)
-    // })
-    // .catch(err => console.log('Initial Call jobs', err))
+export const getJobData = (jobs) => dispatch => {
+    dispatch({ type: C_JOB_DATA })
+    axios
+    .get('', jobs)
+    .then(res => {
+        console.log('Initial Call for jobs', res.data)
+        dispatch({ type: C_JOB_FETCH_SUCCESS, payload:res.data})
+    })
+    .catch(err => console.log('Initial Call jobs', err))
 }
