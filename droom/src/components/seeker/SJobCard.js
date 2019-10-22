363c8f2ca98889291a18ecec34b87081a9dd9fc0
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-22 14:02:04 -0500

    building cards with swipe

--- droom/src/components/seeker/SJobCard.js
+++ droom/src/components/seeker/SJobCard.js
@@ -1,21 +1,49 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Swipeable from "react-swipy";
 
-const cardStyles = {
-  background: "whitesmoke",
-  borderRadius: 3,
-  width: "100%",
-  height: "100%",
-  cursor: "pointer",
-  userSelect: "none",
-  position: "absolute",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  top: 0
-};
-
+const useStyles = makeStyles({
+    card: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
 const Card = ({ zIndex = 0, children }) => (
-  <div style={{ ...cardStyles, zIndex }}>{children}</div>
+  <div style={{ ...useStyles, zIndex }}>{children}</div>
 );
+
+render(){
+    const {cards}=this.state;
+    return(
+        <Swipeable
+               buttons={({ right, left }) => (
+                  <div style={actionsStyles}>
+                    <Button onClick={left}>Reject</Button>
+                    
+                    <Button onClick={right}>Accept</Button>
+                  </div>
+                )}
+                onAfterSwipe={this.remove}
+                >
+                    <Card></Card>
+        </Swipeable>
+    )
+}
 
 export default Card;
