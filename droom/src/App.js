241195365c3e9916df63586d698626ecd206fe1c
CScori <amilanotte1366@gmail.com>
2019-10-22 15:31:11 -0500

    switching to progile

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CJobs from './components/company/CJobs';
 
 function App() {
   return (
     <div className="App">
       <h1>Droom</h1>
+    
     </div>
   );
 }
---
+++ droom/src/utils/actions/index.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+/*<------------------COMPANY JOBS FETCH ---------->*/
+export const C_JOB_DATA = 'C_JOB_DATA'
+export const C_JOB_FETCH_SUCCESS = 'C_JOB_FETCH_SUCCESS'
+export const C_JOB_FETCH_FAILED = 'C_JOB_FETCH_FAILED '
+
+export const getJobData = (jobs) => dispatch => {
+    dispatch({ type: C_JOB_DATA })
+    axios
+    .get('https://droom6.herokuapp.com/api/jobs')
+    .then(res => {
+        console.log('Initial Call for jobs', res.data)
+        dispatch({ type: C_JOB_FETCH_SUCCESS, payload:res.data})
+    })
+    .catch(err => console.log('Initial Call jobs', err))
+}
+/*<------------------EDIT AND DELETE FOR C JOBS ---------->*/
+export const EDIT_C_JOB_DATA = 'EDIT_C_JOB_DATA'
+export const EDIT_C_JOB_DATA_SUCCESS = 'EDIT_C_JOB_DATA_SUCCESS'
+export const editJobData = (id) => dispatch => {
+   dispatch({ type: EDIT_C_JOB_DATA}) 
+   axios
+   .put('', id)
+   .then(res => {
+       console.log('Edited data on job', id)
+       dispatch({ type: EDIT_C_JOB_DATA_SUCCESS, payload: res.data})
+   })
+   .catch(err => console.log('Edit job err', err))
+}
+
+export const DELETE_C_JOB = 'DELETE_C_JOB'
+export const DELETE_C_JOB_FAILED = 'DELETE_C_JOB_FAILED'
+export const deleteJobPost = (id) => dispatch => {
+    dispatch({ type: DELETE_C_JOB });
+    axios
+        .put('')
+        .then(res => {
+            console.log('removing filled position', res.data)
+            dispatch({ type: DELETE_C_JOB, payload: res.data.id })
+        })
+        .catch(err => dispatch({ type: DELETE_C_JOB_FAILED }))
+}
