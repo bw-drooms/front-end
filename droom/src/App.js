042b02f698c5c28c3f4d51cc41eabad069e43c7b
Maddy <mmcintosh2k14@gmail.com>
2019-10-22 16:59:22 -0400

    adds completed navigation component

--- droom/src/App.js
+++ droom/src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import CJobs from './components/company/CJobs'
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import CProfile from './components/company/CProfile'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <Router >
+      <NavBar />
       <div className="App">
         <h1>Droom</h1>
         <Route path="/login" component={LoginForm}/>
         <Route path="/signup" component={SignupForm}/>
+        <Route path="/profile" component={CProfile}/>
+        <Route path="/jobs" component={CJobs}/>
       </div>
     </Router>
   );
