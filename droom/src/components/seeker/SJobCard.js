62072774e52906df3dbf9747031d959c5591cf9b
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-22 14:25:50 -0500

    building

--- droom/src/components/seeker/SJobCard.js
+++ droom/src/components/seeker/SJobCard.js
@@ -1,49 +1,29 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Swipeable from "react-swipy";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core';
+import CardDetails from './MainStreamCard';
 
-const useStyles = makeStyles({
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
-  
-const Card = ({ zIndex = 0, children }) => (
-  <div style={{ ...useStyles, zIndex }}>{children}</div>
-);
+const JobCard = props => {
+    const { classes } = props;
 
-render(){
-    const {cards}=this.state;
-    return(
-        <Swipeable
-               buttons={({ right, left }) => (
-                  <div style={actionsStyles}>
-                    <Button onClick={left}>Reject</Button>
-                    
-                    <Button onClick={right}>Accept</Button>
-                  </div>
-                )}
-                onAfterSwipe={this.remove}
-                >
-                    <Card></Card>
-        </Swipeable>
-    )
-}
+    return (
+        <CardDetails
+            image={props.account.jobImg}
+            title={props.account.jobTitle}
+            subheader={props.account.jobPosition}
+            chips={props.account.jobTags}
+            discription={props.account.jobDescription}
+        />
+    );
+};
 
-export default Card;
+JobCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+    account: PropTypes.object.isRequired,
+};
+
+const styles = {
+    root: {},
+};
+
+export default withStyles(styles)(JobCard);
