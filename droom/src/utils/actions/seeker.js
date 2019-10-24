2cf1df36350404d782fa27ff0a9256e65fbf725e
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-24 13:30:48 -0500

    wow

--- droom/src/utils/actions/seeker.js
+++ droom/src/utils/actions/seeker.js
@@ -1,7 +1,14 @@
-import axiosWithAuth from '../axiosWithAuth'
+import { axiosWithAuth } from '../axiosWithAuth';
+import actionCreator from './actionCreator';
 export const S_JOB_DATA = 'S_JOB_DATA'
 export const S_JOB_FETCH_SUCCESS = 'S_JOB_FETCH_SUCCESS'
 export const S_JOB_FETCH_FAILED = 'S_JOB_FETCH_FAILED '
+export const APPROVE_MATCH = 'APPROVE_MATCH';
+export const MATCHES_FETCHED = 'MATCHES_FETCHED';
+export const MATCH_ERROR = 'MATCH_ERROR';
+export const DISAPPROVE_MATCH = 'DISAPPROVE_MATCH';
+export const FETCHED_CURRENT_MATCHES = 'FETCHED_CURRENT_MATCHES';
+export const CURRENT_MATCHES_ERROR = 'CURRENT_MATCHES_ERROR';
 
 export const getSeekerJobs = (jobs) => dispatch => {
     dispatch({ type: S_JOB_DATA })
@@ -13,3 +20,6 @@ export const getSeekerJobs = (jobs) => dispatch => {
     })
     .catch(err => console.log('Initial Call jobs', err))
 }
+
+
+//////////////match///////////////
